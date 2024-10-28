7623308251619a7d39524d838709a6217aaeab7a
kulanov-ubuntu <kulanov.av@phystech.edu>
2024-10-28 21:47:53 +0300

    Игра жизнь по строкам график готов

---
+++ 3-life/run_life_linux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Loop from 1 to 12
+for i in {1..12}; do
+    echo "\nRunning mpirun with -np $i"
+    sudo time -f "%e" -o times.txt -a mpirun --map-by :OVERSUBSCRIBE -np $i ./life_mpi p46gun_big.cfg
+done
+
+echo "All runs completed. Check times.txt for results."
